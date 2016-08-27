b62fad42dbef1e5b5b35a2928d1bd8ab4bb4dcdf
hp219 <tennisfan13a@gmail.com>
2016-08-27 16:53:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp219/navdrwarmultactivity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.hp219.navdrwarmultactivity;
+
+import android.content.res.TypedArray;
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+    private String[] navMenuTitles;
+    private TypedArray navMenuIcons;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items); // load titles from strings.xml
+
+        navMenuIcons = getResources()
+                .obtainTypedArray(R.array.nav_drawer_icons);//load icons from strings.xml
+
+        set(navMenuTitles,navMenuIcons);
+    }
+}
