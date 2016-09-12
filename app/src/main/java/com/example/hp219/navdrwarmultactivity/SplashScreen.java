b1a62de4c3c8f299118f04597c299979fd2a098b
hp219 <tennisfan13a@gmail.com>
2016-09-12 11:25:23 +0530

    Some xml files are unecessary

---
+++ app/src/main/java/com/example/hp219/navdrwarmultactivity/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.example.hp219.navdrwarmultactivity;
+
+/**
+ * Created by hp219 on 08-09-2016.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by vamsikrishna on 12-Feb-15.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
